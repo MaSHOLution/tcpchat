0892fb0ccf07eca1c1323b90fce1bf9608df9d40
Manuel Schmid <manuel.schmid4@gmx.de>
2015-02-10 22:48:34 +0100

    [TASK] Rebuilt UDP-Class and tests

--- TcpChat/src/networking/methods/Udp.java
+++ TcpChat/src/networking/methods/Udp.java
@@ -28,6 +28,7 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.net.SocketTimeoutException;
 import java.net.UnknownHostException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -36,50 +37,105 @@ import java.util.logging.Logger;
  *
  * @author Manuel Schmid
  */
-public class Udp implements NetworkProtocolClass {
+public class UDP implements NetworkProtocolClass {
 
-    public static final int packetSize = 1024;
+    byte[] inData = new byte[1024]; // Platz für Pakete vorbereiten
+    byte[] outData = new byte[1024];
+    String message;
+    DatagramSocket socket;
+    int senderPort;
+    InetAddress senderIP;
+    Type type;
+    String check = "OK";
 
-    private final byte[] inData = new byte[packetSize];
-    private byte[] outData = new byte[packetSize];
-    private final InetAddress serverIP;
-    private final DatagramSocket socket;
-    private final int port;
-
-    public Udp(String server, int port, Type type) throws UnknownHostException, SocketException {
-        if(type == Type.Client){
-            socket = new DatagramSocket(port);
-        } else {
-            socket = new DatagramSocket();
-        }
-        serverIP = InetAddress.getByName(server);
-        this.port = port;
-    }
-
-    @Override
-    public boolean send(String message) {
+    public UDP(Type type) {
         try {
-            outData = message.getBytes();
-            DatagramPacket out = new DatagramPacket(outData, outData.length, serverIP, port);
-            socket.send(out);
-            return true;
-        } catch (IOException ex) {
-            Logger.getLogger(Udp.class.getName()).log(Level.SEVERE, null, ex);
+            this.type = type;
+            if (type == Type.Client) {
+                senderIP = InetAddress.getByName("localhost");
+                socket = new DatagramSocket(8001);
+            } else {
+                socket = new DatagramSocket(8000);
+            }
+        } catch (SocketException | UnknownHostException ex) {
+            Logger.getLogger(UDP.class.getName()).log(Level.SEVERE, null, ex);
         }
-        return false;
     }
 
     @Override
     public String read() {
         try {
-            DatagramPacket in = new DatagramPacket(inData, inData.length);
-            socket.receive(in);
-            String message = new String(in.getData(), 0, in.getLength());
+            resetSocketTimeout();
+            if (type == Type.Client) {
+                // Antwort empfangen und ausgeben.
+                DatagramPacket in = new DatagramPacket(inData, inData.length);
+                socket.receive(in);
+                message = new String(in.getData(), 0, in.getLength());
+                sendWithoutTimeout(check);
+                //close();
+            } else {
+                // Ein Paket empfangen
+                DatagramPacket in = new DatagramPacket(inData, inData.length);
+                socket.receive(in);
+                // Infos ermitteln und ausgeben
+                senderIP = in.getAddress();
+                senderPort = in.getPort();
+                message = new String(in.getData(), 0, in.getLength());
+                sendWithoutTimeout(check);
+            }
             return message;
         } catch (IOException ex) {
-            Logger.getLogger(Udp.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(UDP.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
+    }
+
+    @Override
+    public boolean send(String message) {
+        try {
+            if (sendWithoutTimeout(message)) {
+                socket.setSoTimeout(5000);
+                while (true) {
+                    try {
+                        DatagramPacket checkPacket = new DatagramPacket(inData, inData.length);
+                        socket.receive(checkPacket);
+                        String received = new String(checkPacket.getData(), 0, checkPacket.getLength());
+                        if(received.equals(check)){
+                            return true;
+                        }                        
+                        // TODO multiclient
+                        return false;
+                    } catch (SocketTimeoutException e) {
+                        System.out.println("Timeout reached!!! " + e);
+                        return false;
+                    }
+                }
+            }
+        } catch (IOException ex) {
+            Logger.getLogger(UDP.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
+    }
+
+    public boolean sendWithoutTimeout(String message) {
+        try {
+            
+            if (type == Type.Client) {
+                //socket = new DatagramSocket();
+                outData = message.getBytes();
+                DatagramPacket out = new DatagramPacket(outData, outData.length, senderIP, 8000);
+                socket.send(out);
+            } else {
+                resetSocketTimeout();
+                outData = message.getBytes();
+                DatagramPacket out = new DatagramPacket(outData, outData.length, senderIP, senderPort);
+                socket.send(out);
+            }
+            return true;
+        } catch (IOException ex) {
+            Logger.getLogger(UDP.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return false;
     }
 
     @Override
@@ -90,5 +146,15 @@ public class Udp implements NetworkProtocolClass {
     @Override
     public void close() {
         socket.close();
+    }
+
+    private void resetSocketTimeout() {
+        try {
+            if (socket != null) {
+                socket.setSoTimeout(0);
+            }
+        } catch (SocketException ex) {
+            Logger.getLogger(UDP.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }

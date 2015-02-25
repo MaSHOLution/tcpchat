709b25092ed503c26d1246b20f0e8eed95f7f5af
Manuel Schmid <manuel.schmid4@gmx.de>
2015-02-25 19:21:13 +0100

    [TASK] Reset for develop

--- TcpChat/src/de/mash1t/chat/client/gui/ClientGui.java
+++ TcpChat/src/de/mash1t/chat/client/gui/ClientGui.java
@@ -88,8 +88,8 @@ public final class ClientGui extends javax.swing.JFrame {
 
         // Set fields to default value
         tbPort.setText(ConfigParam.Port.getDefaultValue());
-        //tbServer.setText("localhost");
-        tbServer.setText("mash-it.org");
+        tbServer.setText("localhost");
+        //tbServer.setText("mash-it.org");
 
         // Initialize a new DialogHelper
         dialogHelper = new DialogHelper(this);
